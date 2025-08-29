43b579200f7b3b3c31445c882aca52e325d36483
Ramana Jhammisetty <ramana.personal.work@gmail.com>
2025-08-29 14:17:38 +0530

    feat: update for sample test deploy and browse

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,39 +1,25 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
-using Microsoft.OpenApi.Models;
+using Application.Interfaces;
 using Infrastructure;
 using Infrastructure.Services;
-using Application.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<AzureStorageSettings>(builder.Configuration.GetSection("AzureStorage"));
-
+// Bind Azure Blob settings
+builder.Services.Configure<AzureStorageSettings>(builder.Configuration.GetSection("AzureBlobStorage"));
 // Add DI for application interface -> infrastructure implementation
 builder.Services.AddScoped<IBlobStorageService, BlobStorageService>();
-
-// Authentication / Authorization (Azure AD)
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-builder.Services.AddAuthorization();
-
-// Swagger with OAuth2 (as earlier)
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => { /* same as earlier */ });
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseSwagger();
-app.UseSwaggerUI(c => {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rules Engine Service v1");
-    c.OAuthClientId(builder.Configuration["AzureAd:ClientId"]);
-    c.OAuthUsePkce();
-});
-
 app.MapControllers();
 app.Run();
